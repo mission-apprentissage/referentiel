158ed387c18d7a887505fd9530a0fea055d271b1
Benoît Guérout <bguerout@gmail.com>
2021-09-22 12:00:21 +0200

    Fix slack conf

--- server/src/config.js
+++ server/src/config.js
@@ -1,11 +1,13 @@
 const env = require("env-var");
 
 module.exports = {
+  env: env.get("REFERENTIEL_ENV").default("local").asString(),
   log: {
     level: env.get("REFERENTIEL_LOG_LEVEL").default("info").asString(),
     format: env.get("REFERENTIEL_LOG_FORMAT").default("pretty").asString(),
     destinations: env.get("REFERENTIEL_LOG_DESTINATIONS").default("stdout").asArray(),
   },
+  slackWebhookUrl: env.get("REFERENTIEL_SLACK_WEBHOOK_URL").asString(),
   mongodb: {
     uri: env
       .get("REFERENTIEL_MONGODB_URI")
