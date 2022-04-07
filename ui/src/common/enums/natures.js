54d8171d294dd8a0d651d89496d23eb082c14287
Benoît Guérout <bguerout@gmail.com>
2022-04-07 14:58:58 +0200

    Mise à jour des couleurs des légendes

--- ui/src/common/enums/natures.js
+++ ui/src/common/enums/natures.js
@@ -1,12 +1,12 @@
 import { sortBy } from "lodash-es";
 
 const MAPPER = {
-  responsable: { label: "Responsable", color: "#88ABF8", order: 0, params: { natures: "responsable" } },
+  responsable: { label: "Responsable", color: "#B4C8FB", order: 0, params: { natures: "responsable" } },
   formateur: { label: "Formateur", color: "#417DC4", order: 2, params: { natures: "formateur" } },
   inconnue: { label: "N.A", color: "#2E4C8C", order: 3, params: { natures: "inconnue" } },
   responsable_formateur: {
     label: "Responsable et formateur",
-    color: "#B4C8FB",
+    color: "#88ABF8",
     order: 1,
     params: { natures: "responsable_formateur" },
   },
