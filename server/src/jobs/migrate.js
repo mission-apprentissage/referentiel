f3f639b3536c9700bde14a25e2e1b7d1b76bc5da
Benoît Guérout <bguerout@gmail.com>
2022-10-03 07:46:18 +0200

    Migration du champs date_maj

--- server/src/jobs/migrate.js
+++ server/src/jobs/migrate.js
@@ -1,12 +1,58 @@
-const { migrateMongodb } = require("../common/db/mongodb.js");
+const { migrateMongodb, dbCollection } = require("../common/db/mongodb.js");
+const { DateTime } = require("luxon");
+const { oleoduc, writeData } = require("oleoduc");
 
 const VERSION = 15;
 
 async function migrate(options = {}) {
+  const twoWeeksAgo = DateTime.now().minus({ day: 15 }).toJSDate();
+
+  async function addDateMaj(path) {
+    const { modifiedCount } = await dbCollection("organismes").updateMany(
+      { [`${path}.0`]: { $exists: true } },
+      { $set: { [`${path}.$[].date_maj`]: twoWeeksAgo } }
+    );
+    return modifiedCount;
+  }
+
+  async function updateAnomalies() {
+    let modifiedCount;
+    await oleoduc(
+      dbCollection("organismes")
+        .find({ "_meta.anomalies": { $exists: true } })
+        .stream(),
+      writeData(async (organisme) => {
+        const res = await dbCollection("organismes").updateOne(
+          { _id: organisme._id },
+          {
+            $set: {
+              "_meta.anomalies": organisme._meta.anomalies.map(({ date, ...rest }) => {
+                return {
+                  ...rest,
+                  date_maj: date,
+                };
+              }),
+            },
+          }
+        );
+        modifiedCount += res.modifiedCount;
+      })
+    );
+
+    return modifiedCount;
+  }
+
   return migrateMongodb(
     VERSION,
-    () => {
-      return {};
+    async () => {
+      return {
+        anomalies: await updateAnomalies(),
+        uai_potentiels: await addDateMaj("uai_potentiels"),
+        contacts: await addDateMaj("contacts"),
+        relations: await addDateMaj("relations"),
+        lieux_de_formation: await addDateMaj("lieux_de_formation"),
+        certifications: await addDateMaj("certifications"),
+      };
     },
     options
   );
