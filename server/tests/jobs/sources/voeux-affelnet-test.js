57c3ab55651261d978084ad2af26968f49041113
Benoit Guérout <bguerout@gmail.com>
2022-10-05 18:12:01 +0200

    Mise à jour de la source voeux-affelnet (#82)

--- server/tests/jobs/sources/voeux-affelnet-test.js
+++ server/tests/jobs/sources/voeux-affelnet-test.js
@@ -6,45 +6,12 @@ const { createStream } = require("../../utils/testUtils");
 const { insertOrganisme } = require("../../utils/fakeData");
 
 describe("voeux-affelnet", () => {
-  it("Vérifie qu'on peut collecter des emails à partir du fichier des voeux-affelnet (uai)", async () => {
-    await insertOrganisme({
-      siret: "11111111100006",
-      uai: "0111111Y",
-    });
-    const source = await createSource("voeux-affelnet", {
-      input: createStream(
-        `email;uai
-"robert@formation.fr";"0111111Y"`
-      ),
-    });
-
-    const stats = await collectSources(source);
-
-    const found = await dbCollection("organismes").findOne({ siret: "11111111100006" }, { _id: 0 });
-    assert.deepStrictEqual(found.contacts, [
-      {
-        email: "robert@formation.fr",
-        confirmé: true,
-        sources: ["voeux-affelnet"],
-      },
-    ]);
-    assert.deepStrictEqual(stats, {
-      "voeux-affelnet": {
-        total: 1,
-        updated: 1,
-        unknown: 0,
-        anomalies: 0,
-        failed: 0,
-      },
-    });
-  });
-
-  it("Vérifie qu'on peut collecter des emails à partir du fichier des voeux-affelnet (siret)", async () => {
+  it("Vérifie qu'on peut collecter des informations à partir du fichier des voeux-affelnet", async () => {
     await insertOrganisme({ siret: "11111111100006" });
     const source = await createSource("voeux-affelnet", {
       input: createStream(
-        `email;uai;siret
-"robert@formation.fr";"0222222X";"11111111100006"`
+        `email;siret;dernier_email
+"robert@formation.fr";"11111111100006";"notification"`
       ),
     });
 
@@ -60,25 +27,18 @@ describe("voeux-affelnet", () => {
     ]);
   });
 
-  it("Vérifie qu'on ne met pas à jour un établisement qu'on le siret est vide", async () => {
-    await insertOrganisme({ siret: "22222222200002" });
+  it("Vérifie qu'on ignore les établissements n'ayant pas activé leur compte", async () => {
+    await insertOrganisme({ siret: "11111111100006" });
     const source = await createSource("voeux-affelnet", {
       input: createStream(
-        `email;uai
-"robert@formation.fr";"0111111Y"`
+        `email;siret;dernier_email
+"robert@formation.fr";"11111111100006";"activation_cfa"`
       ),
     });
 
-    const stats = await collectSources(source);
+    await collectSources(source);
 
-    assert.deepStrictEqual(stats, {
-      "voeux-affelnet": {
-        total: 1,
-        updated: 0,
-        unknown: 1,
-        anomalies: 0,
-        failed: 0,
-      },
-    });
+    const found = await dbCollection("organismes").findOne({ siret: "11111111100006" }, { _id: 0 });
+    assert.deepStrictEqual(found.contacts, []);
   });
 });
