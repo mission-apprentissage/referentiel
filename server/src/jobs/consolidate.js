041c252202f6c2801c6243314f3f6e7eab712c50
Benoît Guérout <bguerout@gmail.com>
2022-10-12 16:14:45 +0200

    Suppression des organismes obsolètes

--- server/src/jobs/consolidate.js
+++ server/src/jobs/consolidate.js
@@ -1,36 +1,10 @@
 const logger = require("../common/logger").child({ context: "consolidate" });
-const { dbCollection } = require("../common/db/mongodb");
-const findUAIProbable = require("../common/actions/findUAIProbable");
-const { addModification } = require("../common/actions/addModification.js");
 const { DateTime } = require("luxon");
+const { dbCollection } = require("../common/db/mongodb");
 const { getLatestCollectDate } = require("../common/actions/getLatestCollectDate.js");
+const { getLatestImportDate } = require("../common/actions/getLatestImportDate.js");
 
-/**
- * Permet de validation automatiquement les UAI sans passer par la validation manuelle
- */
-// eslint-disable-next-line no-unused-vars
-async function validateAllUAI() {
-  const stats = { total: 0, modifications: 0, unknown: 0, failed: 0 };
-  for await (const organisme of dbCollection("organismes").find().stream()) {
-    try {
-      stats.total++;
-      const probable = findUAIProbable(organisme);
-
-      if (probable) {
-        await addModification("consolidation", organisme, { uai: probable.uai });
-        stats.modifications++;
-      } else {
-        stats.unknown++;
-      }
-    } catch (e) {
-      logger.error(e, `Impossible de modifier l'uai pour l'organisme ${organisme.siret}`);
-      stats.failed++;
-    }
-  }
-  return stats;
-}
-
-async function removeObsoleteData() {
+async function removeObsoleteCollectedData() {
   const latestCollectDate = await getLatestCollectDate();
   const $obsolete = { $lt: DateTime.fromJSDate(latestCollectDate).minus({ day: 7 }).toJSDate() };
 
@@ -52,6 +26,18 @@ async function removeObsoleteData() {
   );
 
   return modifiedCount;
+}
+
+async function removeObsoleteOrganismes() {
+  const importDate = await getLatestImportDate();
+  const obsoleteDate = DateTime.fromJSDate(importDate).minus({ day: 7 }).toJSDate();
+
+  const { deletedCount } = await dbCollection("organismes").deleteMany({
+    "uai": { $exists: false },
+    "_meta.date_dernier_import": { $lt: obsoleteDate },
+  });
+
+  return deletedCount;
 }
 
 async function applyModifications(options = {}) {
@@ -78,10 +64,13 @@ async function applyModifications(options = {}) {
 }
 
 async function consolidate(options) {
-  const stats = {};
-  stats.obsolete = await removeObsoleteData();
-  stats.modifications = await applyModifications(options);
-  return stats;
+  return {
+    obsolete: {
+      collecte: await removeObsoleteCollectedData(),
+      organismes: await removeObsoleteOrganismes(),
+    },
+    modifications: await applyModifications(options),
+  };
 }
 
 module.exports = consolidate;
