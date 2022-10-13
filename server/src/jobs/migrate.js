bd07beb979e2170429602f033a06004b2a1e1fd1
Benoît Guérout <bguerout@gmail.com>
2022-10-13 22:33:41 +0200

    Mise à jour du script de migration

--- server/src/jobs/migrate.js
+++ server/src/jobs/migrate.js
@@ -1,4 +1,7 @@
 const { migrateMongodb, dbCollection } = require("../common/db/mongodb.js");
+const { createSource } = require("./sources/sources.js");
+const importOrganismes = require("./importOrganismes.js");
+const consolidate = require("./consolidate.js");
 
 const VERSION = 16;
 
@@ -6,10 +9,19 @@ async function migrate(options = {}) {
   return migrateMongodb(
     VERSION,
     async () => {
+      const dernierImport = await dbCollection("organismes").updateMany({}, [
+        { $set: { "_meta.date_dernier_import": "$_meta.date_import" } },
+      ]);
+
+      const sources = ["catalogue-etablissements", "sifa-ramsese", "datagouv"].map((name) => createSource(name));
+      const stats = {
+        imports: await importOrganismes(sources),
+        consolidate: await consolidate(),
+      };
+
       return {
-        dernier_import: await dbCollection("organismes").updateMany({}, [
-          { $set: { "_meta.date_dernier_import": "$_meta.date_import" } },
-        ]),
+        dernier_import: dernierImport,
+        ...stats,
       };
     },
     options
