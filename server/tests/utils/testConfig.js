6f96c6ab929d1c73879dcb8be720336544d6dd0d
Samir BENFARES <sbenfares@gmail.com>
2023-03-15 14:51:14 +0100

    chore(server): update testConfig

--- server/tests/utils/testConfig.js
+++ server/tests/utils/testConfig.js
@@ -5,3 +5,5 @@ process.env.REFERENTIEL_API_ACCE_USERNAME = "username";
 process.env.REFERENTIEL_API_ACCE_PASSWORD = "secret";
 process.env.TABLEAU_DE_BORD_API_KEY = "123456789";
 process.env.TABLEAU_DE_BORD_PUBLIC_URL = "https://cfas.apprentissage.beta.gouv.fr/api";
+process.env.CATALOGUE_API_USERNAME = "referentiel-user";
+process.env.CATALOGUE_API_PASSWORD = "CatalogApiPasswordTest";
