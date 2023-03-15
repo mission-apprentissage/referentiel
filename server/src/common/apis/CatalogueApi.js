0937686c79596cf5be32616c246824c38e74afd6
Samir BENFARES <sbenfares@gmail.com>
2023-03-15 14:32:18 +0100

    chore(server): add cookie authenticated call to catalogue API

--- server/src/common/apis/CatalogueApi.js
+++ server/src/common/apis/CatalogueApi.js
@@ -3,20 +3,47 @@ const { fetchStream, fetchData } = require("../utils/httpUtils");
 const { compose } = require("oleoduc");
 const convertQueryIntoParams = require("./utils/convertQueryIntoParams");
 const { streamJsonArray } = require("../utils/streamUtils");
+const config = require("../../config");
+const logger = require("../logger.js");
 
 class CatalogueApi extends RateLimitedApi {
   constructor(options = {}) {
     super("CatalogueApi", { nbRequests: 5, perSeconds: 1, ...options });
+    this.authCookie = null;
   }
 
   static get baseApiUrl() {
-    return "https://catalogue-apprentissage.intercariforef.org/api";
+    return "https://catalogue.apprentissage.education.gouv.fr/api";
+  }
+
+  async login() {
+    const response = await fetchData(`${CatalogueApi.baseApiUrl}/v1/auth/login`, {
+      method: "POST",
+      raw: true,
+      data: {
+        username: config.api.catalogue.username,
+        password: config.api.catalogue.password,
+      },
+    });
+
+    logger.info(`Le cookie d'authentification a été renouvelé`);
+    this.authCookie = response.headers["set-cookie"].join(";");
+  }
+
+  isAuthenticated() {
+    return !!this.authCookie;
   }
 
   streamFormations(query, options) {
     return this.execute(async () => {
+      if (!this.isAuthenticated()) {
+        await this.login();
+      }
+
       const params = convertQueryIntoParams(query, options);
-      const response = await fetchStream(`${CatalogueApi.baseApiUrl}/entity/formations.json?${params}`);
+      const response = await fetchStream(`${CatalogueApi.baseApiUrl}/entity/formations.json?${params}`, {
+        headers: { cookie: this.authCookie },
+      });
 
       return compose(response, streamJsonArray());
     });
@@ -24,9 +51,14 @@ class CatalogueApi extends RateLimitedApi {
 
   streamEtablissements(query, options) {
     return this.execute(async () => {
-      const params = convertQueryIntoParams(query, options);
+      if (!this.isAuthenticated()) {
+        await this.login();
+      }
 
-      const response = await fetchStream(`${CatalogueApi.baseApiUrl}/entity/etablissements.json?${params}`);
+      const params = convertQueryIntoParams(query, options);
+      const response = await fetchStream(`${CatalogueApi.baseApiUrl}/entity/etablissements.json?${params}`, {
+        headers: { cookie: this.authCookie },
+      });
 
       return compose(response, streamJsonArray());
     });
@@ -34,9 +66,14 @@ class CatalogueApi extends RateLimitedApi {
 
   getEtablissement(query, options) {
     return this.execute(async () => {
-      const params = convertQueryIntoParams(query, options);
+      if (!this.isAuthenticated()) {
+        await this.login();
+      }
 
-      return fetchData(`${CatalogueApi.baseApiUrl}/entity/etablissement?${params}`);
+      const params = convertQueryIntoParams(query, options);
+      return fetchData(`${CatalogueApi.baseApiUrl}/entity/etablissement?${params}`, {
+        headers: { cookie: this.authCookie },
+      });
     });
   }
 }
