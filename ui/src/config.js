d87e56349e62683594373984b7f3acab6d4896d6
Georges-André Silber <georges-andre.silber@minesparis.psl.eu>
2023-06-13 07:32:29 +0200

    Default for REACT_APP_REFERENTIEL_API_URL

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,5 +1,5 @@
 const env = require("env-var");
 
 module.exports = {
-  apiUrl: env.get("REACT_APP_REFERENTIEL_API_URL").default("http://localhost:5001/api/v1").asString(),
+  apiUrl: env.get("REACT_APP_REFERENTIEL_API_URL").default("/api/v1").asString(),
 };
