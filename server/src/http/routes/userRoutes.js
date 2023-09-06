eb0fc31ecdaa8617aff588b7a919d19a3a4fc52a
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-09-06 17:12:06 +0200

    feat(user): send cookie when login

--- server/src/http/routes/userRoutes.js
+++ server/src/http/routes/userRoutes.js
@@ -3,6 +3,19 @@ const tryCatch = require("../middlewares/tryCatchMiddleware");
 const { verifyUser } = require("../middlewares/authMiddleware");
 const { buildJwtToken, buildRefreshToken } = require("../../common/utils/jwtUtils");
 const { dbCollection } = require("../../common/db/mongodb");
+const config = require("../../config");
+
+const isDev = config.env === "local";
+
+const { refreshTokenExpiry } = config.auth.api;
+
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: !isDev,
+  signed: true,
+  maxAge: refreshTokenExpiry * 1000,
+  sameSite: "strict",
+};
 
 module.exports = () => {
   const router = express.Router();
@@ -14,13 +27,14 @@ module.exports = () => {
       const user = req.user;
 
       const token = buildJwtToken(user.email, user.type, user.code);
-      const refreshToken = buildRefreshToken(user);
+      const refreshToken = buildRefreshToken(user.email, user.type, user.code);
 
       const userRefreshTokens = user.refreshToken || [];
       userRefreshTokens.push(refreshToken);
 
       await dbCollection("users").updateOne({ email: user.email }, { $set: { refreshToken: userRefreshTokens } });
 
+      res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
       res.json({ success: true, token });
     })
   );
