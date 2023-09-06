abd1fd4a35311ecc1c917ddeb6d5bbd0aa913315
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-09-06 16:34:24 +0200

    fix(addUser): hash password

--- server/src/common/actions/addUser.js
+++ server/src/common/actions/addUser.js
@@ -5,10 +5,11 @@ const config = require("../../config");
 const addUser = (email, password, type, code) => {
   const salt = config.auth.api.salt;
 
-  crypto.pbkdf2(password, salt, 310000, 32, "sha256", (err, hashedPassword) => {
+  crypto.pbkdf2(password, salt, 310000, 32, "sha256", (err, encryptedPassword) => {
     if (err) {
       throw new Error("Error while deriving key");
     }
+    const hashedPassword = encryptedPassword.toString("hex");
 
     return dbCollection("users").insertOne({
       email,
