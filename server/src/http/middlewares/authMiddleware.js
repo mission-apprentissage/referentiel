b88aee79a37f60484a2f625abe1c51f609f13247
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-09-07 10:28:15 +0200

    refacto(auth): remove legacy auth

--- server/src/http/middlewares/authMiddleware.js
+++ server/src/http/middlewares/authMiddleware.js
@@ -1,9 +1,5 @@
-const config = require("../../config");
 const passport = require("passport");
 const { Strategy: AnonymousStrategy } = require("passport-anonymous");
-const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
-const { findRegionByCode } = require("../../common/regions");
-const { findAcademieByCode } = require("../../common/academies");
 
 const STRATEGIES = {
   local: "local",
@@ -11,44 +7,6 @@ const STRATEGIES = {
 };
 
 passport.use(new AnonymousStrategy());
-passport.use(
-  "api-token",
-  new JwtStrategy(
-    {
-      secretOrKey: config.auth.api.jwtSecret,
-      ignoreExpiration: true,
-      passReqToCallback: true,
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        ExtractJwt.fromUrlQueryParameter("token"),
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ]),
-    },
-    (req, jwt_payload, done) => {
-      const code = jwt_payload.sub;
-      const type = jwt_payload.type;
-      const found = type === "region" ? findRegionByCode(code) : findAcademieByCode(code);
-
-      if (found) {
-        const user = { type, code, departements: found.departements };
-        done(null, user);
-      } else {
-        done(null, false);
-      }
-    }
-  )
-);
-
-function checkOptionnalApiToken() {
-  return passport.authenticate(["api-token", "anonymous"], {
-    session: false,
-    failWithError: true,
-    assignProperty: "user",
-  });
-}
-
-function checkApiToken() {
-  return passport.authenticate("api-token", { session: false, failWithError: true, assignProperty: "user" });
-}
 
 const passportCallback = (req, res, next) => {
   return (error, user) => {
@@ -68,8 +26,6 @@ const verifyUser = (req, res, next) => {
 };
 
 module.exports = {
-  checkApiToken,
-  checkOptionnalApiToken,
   verifyUser,
   STRATEGIES,
   passportCallback,
