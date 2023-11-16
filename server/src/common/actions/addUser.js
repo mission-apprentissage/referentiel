7190f6b8011c8fab190ed73db44ea2a91172227f
Yohann Gablowski <yohann.gablowski@gmail.com>
2023-11-16 11:19:22 +0100

    fix(addUser): correctly return async

--- server/src/common/actions/addUser.js
+++ server/src/common/actions/addUser.js
@@ -2,10 +2,10 @@ const crypto = require("crypto");
 const { dbCollection } = require("../../common/db/mongodb");
 const config = require("../../config");
 
-const addUser = (email, password, type, code) => {
+const addUser = async (email, password, type, code) => {
   const salt = config.auth.api.salt;
 
-  crypto.pbkdf2(password, salt, 310000, 32, "sha256", (err, encryptedPassword) => {
+  return crypto.pbkdf2(password, salt, 310000, 32, "sha256", async (err, encryptedPassword) => {
     if (err) {
       throw new Error("Error while deriving key");
     }
