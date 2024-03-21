e1dc537c366cc3217239f00b548fcbd34a96ed53
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-03-21 15:35:51 +0100

    feat(rateLimiter): up limit

--- server/src/http/middlewares/rateLimiterMiddleware.js
+++ server/src/http/middlewares/rateLimiterMiddleware.js
@@ -2,9 +2,9 @@ const RateLimiter = require("../../common/apis/RateLimiter");
 const config = require("../../config");
 
 const loginRateLimiter = new RateLimiter("user", {
-  nbRequests: config.env === "local" ? 100000 : 5,
+  nbRequests: config.env === "local" ? 100000 : 25,
   perSeconds: 60,
-  maxQueueSize: 10,
+  maxQueueSize: 25,
 });
 
 const rateLimitMiddleware = async (req, res, next) => {
