24b5ba9d109552eb877464276a7fb9e97d5f6296
Yohann Gablowski <yohann.gablowski@gmail.com>
2024-12-12 15:19:11 +0100

    feat(correction): wording page correction

--- ui/src/CorrectionsPage.jsx
+++ ui/src/CorrectionsPage.jsx
@@ -20,7 +20,13 @@ export default function CorrectionsPage() {
       <ContentLayout>
         <GridRow modifiers={"gutters"} className={"fr-pb-3w"}>
           <Col modifiers={"12"}>
-            <Highlight>Le Référentiel est une photographie à l’instant de consultation !</Highlight>
+            <Highlight>
+              Il se peut que certaines données affichées sur le Référentiel soient erronées ou que des données soient
+              manquantes. Cette page vous donne des indications sur la procédure à suivre pour les corriger ou les
+              compléter.
+              <br /> Pour tout autres signalements, vous pouvez contacter l’équipe gestionnaire du site à l’adresse
+              suivante : referentiel-uai-siret@onisep.fr
+            </Highlight>
           </Col>
         </GridRow>
         <GridRow modifiers={"gutters"} className={"fr-pb-3w"}>
@@ -36,66 +42,46 @@ export default function CorrectionsPage() {
                   </Alert>
                   <div className={"fr-mt-3w"}>
                     <span className={"fr-text--bold fr-pr-1v"}>
-                      Si un organisme a pour nature "N.A" (inconnue), c’est que l'offre de formation n’est pas collectée
+                      Si un organisme est de Nature inconnue, c’est qu’il n’y a pas d’offre de formation collectée
                     </span>
-                    par le Carif-Oref, il faudra donc dans un premier temps vérifier auprès de RCO si une modification
-                    de la collecte est possible :
-                    <ul>
-                      <li>si oui, le CSAIO devra demander à son Carif-Oref de collecter les natures manquantes</li>
-                      <li>si non, la correction devra se faire directement dans l’interface du Référentiel</li>
-                      <ul>
-                        <li>
-                          <i>
-                            aujourd’hui un seul cas s’est présenté, lorsque 3 niveaux d’organismes sont détectés (alors
-                            que la collecte gère aujourd’hui seulement 2 niveaux : responsable > formateur), alors au
-                            moins un des organisme a un nature inconnue quelque soit le niveau où il se trouve
-                          </i>
-                        </li>
-                      </ul>
-                    </ul>
+                    Si un organisme est de Nature inconnue, c’est qu’il n’y a pas d’offre de formation collectée par le
+                    Carif-Oref pour cet établissement. Aucune nature n’a pu être déduite. Il faut donc vérifier auprès
+                    du Carif-Oref d’appartenance qu’une modification de la collecte est possible. Si oui, le Carif-Oref
+                    devra alors référencer l’offre de formation des organismes manquants. La collecte de cette offre
+                    permettra de déduire la nature de l’organisme.
                   </div>
 
                   <div className={"fr-mt-3w"}>
                     <span className={"fr-text--bold fr-pr-1v"}>
-                      Si un organisme à une nature incorrecte, c’est que l'offre de formation est mal collectée
+                      Si un organisme à une Nature incorrecte, c’est que l’offre de formation est mal collectée
                     </span>
-                    par le Carif-Oref, il faudra donc dans un premier temps vérifier auprès de RCO si une modification
-                    de la collecte est possible :
-                    <ul>
-                      <li>si oui, le CSAIO devra demander à son Carif-Oref de modifier les natures incorrectes</li>
-                      <li>si non, la correction devra se faire directement dans l’interface du Référentiel</li>
-                      <ul>
-                        <li>
-                          <i>
-                            aujourd’hui un seul cas s’est présenté, lorsque 3 niveaux d’organismes sont détectés (alors
-                            que la collecte gère aujourd’hui seulement 2 niveaux : responsable > formateur), alors les
-                            natures sont mal réparties et par exemple le responsable se retrouve au niveau du formateur
-                            et le formateur inconnu.
-                          </i>
-                        </li>
-                      </ul>
-                    </ul>
+                    par le Carif-Oref pour cet établissement. Il faut donc vérifier auprès du Carif-Oref d’appartenance
+                    qu’une modification de la collecte est possible. Si oui, le Carif-Oref devra alors modifier la
+                    collecte de l’offre existante afin de corriger les natures incorrectes.
+                    <br /> Ex : un organisme de formation est de nature Responsable et formateur dans le Référentiel,
+                    mais en réalité il n’accueille pas d’apprentis et gère simplement les conventions de formation en
+                    apprentissage. Il devrait donc être de nature Responsable uniquement. Dans ce cas précis, la
+                    collecte de l’offre devra être modifiée pour que cet établissement n’apparaisse pas comme organisme
+                    formateur au niveau de l’offre de formation. La nature sera alors automatiquement corrigée dans le
+                    Référentiel.
                   </div>
                 </div>
               </AccordionItem>
               <AccordionItem collapsed={item === "relations"} label={"Des relations entre organismes sont incorrectes"}>
                 <div>
                   <p className={"fr-mt-3w"}>
-                    <span className={"fr-text--bold fr-pr-1v"}>
-                      Si des relations entre organismes ne devraient pas avoir lieu
-                    </span>
-                    (ex : MFR rattaché à un CFA EN), le CSAIO devra demander à son Carif-Oref une modification de la
-                    collecte (par ex : suppression du formateur rattaché au responsable).
+                    <span className={"fr-text--bold fr-pr-1v"}>Si des relations entre organismes sont erronées,</span>
+                    il faut demander une modification de la collecte auprès du Carif-Oref d’appartenance (par ex :
+                    suppression de l’organisme formateur rattaché au responsable).
                   </p>
                   <p>
                     <span className={"fr-text--bold fr-pr-1v"}>
                       Si des relations sont incorrectes aujourd’hui mais historiquement correctes,
                     </span>
                     aucune action n’est possible pour le moment.
-                    <span className={"fr-text--bold fr-pr-1v"}>
-                      Une expression de besoin sera transmise au RCO pour que leur nouvel
-                    </span>
-                    annuaire des établissements QUIFORME introduise la notion d’historique des natures et des relations.
+                    <p className={"fr-pr-1v"}>
+                      <i>Etude en cours côté ONISEP.</i>
+                    </p>
                   </p>
                 </div>
               </AccordionItem>
@@ -105,30 +91,15 @@ export default function CorrectionsPage() {
               >
                 <div>
                   <p className={"fr-mt-3w"}>
-                    <span className={"fr-text--bold fr-pr-1v"}>S’il s’agit d’une erreur sur l’adresse,</span> le CSAIO
-                    devra demander à son Carif-Oref une modification de l’adresse postale du lieu de formation concerné.
+                    <span className={"fr-text--bold fr-pr-1v"}>S’il s’agit d’une erreur sur l’adresse,</span> il faut
+                    demander au Carif-Oref d’appartenance une modification de l’adresse postale du lieu de formation
+                    concerné.
                   </p>
                   <div>
-                    <span className={"fr-text--bold fr-pr-1v"}>S’il s’agit d’une erreur sur UAI,</span> le CSAIO peut
-                    faire la modification directement dans le Catalogue des formations en apprentissage :
-                    <ol>
-                      <li>
-                        Utiliser la recherche avancée pour trouver le lieu de formation :
-                        <ul>
-                          <li>indiquer SIRET de l’organisme (gestionnaire ou formateur)</li>
-                          <li>indiquer la commune du lieu de formation égale à (recherche stricte)</li>
-                        </ul>
-                      </li>
-                      <li>
-                        Utiliser la liste qui s’affiche qui contient potentiellement les formations sur lesquelles
-                        modifier l’UAI du lieu de formation
-                        <img
-                          width={"75%"}
-                          alt={"Page du catalogue des formations en apprentissage"}
-                          src={`${process.env.PUBLIC_URL}/catalogue-page.png`}
-                        ></img>
-                      </li>
-                    </ol>
+                    <span className={"fr-text--bold fr-pr-1v"}>
+                      S’il s’agit d’une erreur sur un UAI lieu de formation,
+                    </span>{" "}
+                    merci d’adresser votre signalement à l’adresse suivante : referentiel-uai-siret@onisep,fr
                   </div>
                 </div>
               </AccordionItem>
@@ -141,6 +112,35 @@ export default function CorrectionsPage() {
                     Si un organisme ne devrait pas être présent dans le Référentiel (par exemple s’il s’agit d’une école
                     maternelle ou élémentaire), merci de le signaler à l’adresse mail suivante :
                     referentiel-uai-siret@onisep.fr pour demander le retrait des listes de cet organisme.
+                  </p>
+                </div>
+              </AccordionItem>
+              <AccordionItem collapsed={item === "erronee"} label={"Une UAI validée dans le Référentiel est erronée"}>
+                <div>
+                  <p className={"fr-mt-3w"}>
+                    <b>Si une UAI validée dans le Référentiel vous semble erronée,</b> merci d’adresser votre
+                    signalement à l’équipe gestionnaire du site à l’adresse suivante : referentiel-uai-siret@onisep.fr
+                    <br />
+                    Une correction pourra se faire après validation par les référents en académie chargés de la
+                    fiabilisation des couples UAI-SIRET.
+                  </p>
+                </div>
+              </AccordionItem>
+              <AccordionItem
+                collapsed={item === "appartenance"}
+                label={"L’appartenance à un réseau est erronée ou manquante"}
+              >
+                <div>
+                  <p className={"fr-mt-3w"}>
+                    L’appartenance à un réseau est une donnée qui vient du{" "}
+                    <a href="https://cfas.apprentissage.beta.gouv.fr" target="_blank" rel="noreferrer">
+                      Tableau de bord de l’Apprentissage
+                    </a>
+                    . Si cette donnée est erronée ou manquante, merci de contacter l’équipe support via le formulaire
+                    suivant :{" "}
+                    <a href="https://cfas.apprentissage.beta.gouv.fr/contact" target="_blank" rel="noreferrer">
+                      https://cfas.apprentissage.beta.gouv.fr/contact
+                    </a>
                   </p>
                 </div>
               </AccordionItem>
