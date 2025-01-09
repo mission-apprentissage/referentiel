f00faee430565732ecff24dc7e987171540e1014
Yohann Gablowski <yohann.gablowski@gmail.com>
2025-01-09 16:08:54 +0100

    feat(Accueil): change wording

--- ui/src/accueil/AccueilPage.jsx
+++ ui/src/accueil/AccueilPage.jsx
@@ -2,16 +2,15 @@ import { Col, GridRow } from "../common/dsfr/fondamentaux/index.js";
 import React, { useContext } from "react";
 import TitleLayout from "../common/layout/TitleLayout.jsx";
 import ContentLayout from "../common/layout/ContentLayout.jsx";
-import { Summary } from "../common/dsfr/elements/Summary.jsx";
 import { Button } from "../common/dsfr/elements/Button.jsx";
 import { useNavigate } from "react-router-dom";
 import { useFetch } from "../common/hooks/useFetch.js";
 import { percentage } from "../common/utils.js";
 import Page from "../common/Page.jsx";
-import { modifications } from "../ModificationsPage.jsx";
 import NouveauxHistogram from "./stats/NouveauxHistogram.jsx";
 import EtatAdministratifPie from "./stats/EtatAdministratifPie.jsx";
 import { UserContext } from "../common/UserProvider.jsx";
+import { Link } from "../common/dsfr/elements/Link.jsx";
 const config = require("../config");
 
 export default function AccueilPage() {
@@ -19,45 +18,127 @@ export default function AccueilPage() {
   const [userContext] = useContext(UserContext);
 
   const [{ data: stats }] = useFetch(config.apiUrl + "/stats/couverture", null);
-
+  console.log({ stats });
   return (
     <Page>
       <TitleLayout />
       <ContentLayout>
         <GridRow modifiers={"gutters"} className={"fr-pb-3w"}>
-          <Col modifiers={"sm-3"} className={"xfr-display-none xfr-display-sm-block"}>
-            <Summary>
-              <a href={"#référentiel"}>Référentiel national</a>
-              <a href={"#construction"}>Construction du Référentiel</a>
-              <a href={"#modifications"}>Journal des modifications</a>
-            </Summary>
-          </Col>
-          <Col modifiers={"12 offset-sm-1 sm-8"}>
+          <Col modifiers={"12 sm-12"}>
             <GridRow modifiers={"gutters"} className={"fr-mb-6w"}>
               <Col modifiers={"12"}>
                 <h2>Bienvenue sur le Référentiel des organismes de formation en apprentissage</h2>
                 <h6 id={"référentiel"}>
-                  Aujourd’hui, des travaux d'expertise sont en cours dans chaque académie.
-                  {stats && (
-                    <div>
-                      Il contient {Math.round(percentage(stats.valides, stats.total))}% d’organismes validés sur le
-                      territoire national.
-                    </div>
-                  )}
+                  Aujourd’hui, le Référentiel comptabilise {stats?.organismes || "0"} organismes.
                 </h6>
-                {stats && (
-                  <div className={"fr-mt-3w fr-text--bold"}>
-                    Les {stats.total} organismes référencés dans le Référentiel sont :
-                  </div>
-                )}
-                <ol>
-                  <li>identifiés par un SIRET</li>
+                <p>
+                  Un établissement, identifié via son SIRET, intègre le Référentiel dès lors qu’il est présent dans
+                  l’une de nos deux sources de référence, à savoir :
+                </p>
+                <ul>
                   <li>
-                    trouvés dans la Liste publique des Organismes de Formation (Data.gouv) et le Catalogue des
-                    formations en apprentissage (base des Carif-Oref)
+                    <a
+                      href={
+                        "https://www.data.gouv.fr/fr/datasets/liste-publique-des-organismes-de-formation-l-6351-7-1-du-code-du-travail/"
+                      }
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      La liste publique des organismes de formation (DGEFP)
+                    </a>
                   </li>
-                  <li>en lien avec des formations en apprentissage à un moment donné</li>
-                </ol>
+                  <li>
+                    Et/ou le{" "}
+                    <a href={"https://catalogue-apprentissage.intercariforef.org/"} target="_blank" rel="noreferrer">
+                      Catalogue des formations en apprentissage
+                    </a>{" "}
+                    (réseau des Carif-Oref)
+                  </li>
+                </ul>
+                <br />
+                <p>
+                  Parmi ces organismes, un{" "}
+                  <span style={{ color: "red" }}>périmètre est identifié comme étant prioritaire</span> pour la
+                  fiabilisation des couples UAI-SIRET.
+                </p>
+                <p>
+                  {stats?.total || "0"} organismes sont aujourd’hui concernés car ils répondent aux critères suivants :
+                </p>
+                <ul>
+                  <li>ils sont identifiés via un SIRET ouvert</li>
+                  <li>
+                    ils sont certifiés Qualiopi par le{" "}
+                    <a
+                      href="https://travail-emploi.gouv.fr/qualiopi-marque-de-certification-qualite-des-prestataires-de-formation"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Cofrac
+                    </a>{" "}
+                    pour dispenser des formations en apprentissage
+                  </li>
+                  <li>
+                    ils ont une nature : Ils peuvent être responsables / responsables et formateurs ou formateurs
+                    uniquement. Cette nature est déduite dès lors qu’ ils sont liés à de l’offre de formation dans le
+                    Catalogue des formations apprentissage (réseau des Carif-Oref).
+                  </li>
+                </ul>
+                <p>
+                  <b>
+                    {Math.round(percentage(stats?.valides, stats?.total))}% des organismes intégrant ce périmètre
+                    d’expertise ont aujourd’hui une UAI fiabilisée.
+                  </b>
+                </p>
+                <p>
+                  Toutefois, il faut noter que la fiabilisation des UAI reste possible sur l’ensemble des organismes
+                  présents dans le Référentiel, ce qui explique qu’aujourd’hui, {stats?.uaiExistant} organismes ont une
+                  UAI validée.
+                </p>
+                <p>
+                  <u>Rôle et usage du Référentiel</u>
+                </p>
+                <p>
+                  Le Référentiel est un des pivots d’enrichissement du Catalogue des formations en apprentissage (réseau
+                  des Carif-Oref). Les UAI fiabilisés dans le Référentiel alimentent chaque jour le Catalogue des
+                  formations en apprentissage. Ces mêmes UAI sont ensuite exploités, via le Catalogue, par les
+                  Ministères éducatifs dans leurs applicatifs Parcoursup et Affelnet.
+                  <br /> Le Tableau de bord de l’Apprentissage (MIA) exploite également les données du Référentiel pour
+                  approuver les organismes de formation souhaitant déclarer leurs effectifs. L’ouverture d’un compte sur
+                  le Tableau de bord est possible uniquement si l’organisme dispose d’un couple UAI-SIRET valide dans le
+                  Référentiel.
+                  <br /> D’autres consommateurs du Référentiel peuvent également être mentionnés comme la Caisse des
+                  dépôts et consignation (CDC) qui appelle les couples SIRET-UAI pour fiabiliser la collecte de la taxe
+                  d’apprentissage ou encore SIRIUS qui exploite les relations entre les organismes.
+                  <br />
+                  Les données de ce site sont sous{" "}
+                  <a
+                    href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    licence etalab-2.0
+                  </a>
+                  . Elles sont librement réutilisables.
+                </p>
+                <p>
+                  <u>Fiabilisation des UAI</u>
+                </p>
+                <p>
+                  L’UAI est la seule donnée modifiable au sein du Référentiel.
+                  <br />
+                  Toutes les autres données qui composent une fiche organisme sont issues des différentes sources que
+                  nous exploitons (INSEE, Catalogue des formations en apprentissage, Liste publique des organismes de
+                  formation…).
+                  <br />
+                  La fiabilisation des couples UAI-SIRET est réalisée par un réseau d’experts travaillant principalement
+                  au sein des services statistiques académiques (SSA) rattachés au niveau national à la Direction de
+                  l’évaluation, de la prospective et de la performance (DEPP).
+                  <br /> Au sein de leur académie, ces experts sont chargés de l’immatriculation des organismes de
+                  formation et de la gestion du répertoire Ramsese. Ils ont donc une véritable connaissance des
+                  organismes de formation de leur territoire.
+                  <br /> Les gestionnaires du site peuvent également être amenés à fiabiliser des UAI au sein du
+                  Référentiel.
+                </p>
                 <Button className={"fr-mt-3w"} onClick={() => navigate("/organismes")}>
                   Consulter le Référentiel national
                 </Button>
@@ -93,22 +174,6 @@ export default function AccueilPage() {
                   </Col>
                 </>
               )}
-            </GridRow>
-
-            <GridRow modifiers={"gutters"}>
-              <Col modifiers={"12"}>
-                <h6 id={"modifications"}>Dernière modification</h6>
-                <div className={"fr-mt-3w"}>{modifications[0]}</div>
-                <Button
-                  modifiers={"secondary"}
-                  onClick={() => {
-                    window.scrollTo(0, 0);
-                    return navigate("/modifications");
-                  }}
-                >
-                  Voir toutes les modifications
-                </Button>
-              </Col>
             </GridRow>
           </Col>
         </GridRow>
